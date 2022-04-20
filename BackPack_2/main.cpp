4e178b417655f719ba22f6de88ee593a15d033ff
amdpayzeal <andrei@payzeal.co>
2022-04-20 12:55:06 +0300

    Initial Commit

---
+++ BackPack_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BackPack_2
+//
+//  Created by Егор Сипачев on 20.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
