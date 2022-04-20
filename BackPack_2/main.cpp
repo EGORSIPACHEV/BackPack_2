0ff47d429ea5c3b30890bea6d131b110f4d6d860
EVSipachev <egor@payzeal.co>
2022-04-20 16:38:19 +0300

    Commit 2

--- BackPack_2/main.cpp
+++ BackPack_2/main.cpp
@@ -7,8 +7,33 @@
 
 #include <iostream>
 
+
+//Жадный алгоритм, один внутри другого, нужно отсортировать по весу и соответствующему индексу
+/*
+Arrays.sort(tasks); //сортируем по убыванию стоимости
+TreeSet<Integer> time = new TreeSet<Integer>();
+for (int i = 1; i <= n; ++i) {
+  time.add(i);
+}
+int ans = 0;
+for (int i = 0; i < n; ++i) {
+  Integer tmp = time.floor(tasks[i].time);
+  if (tmp == null) { // если нет свободного места в расписании, то в конец
+  time.remove(time.last());
+  } else { //иначе можно выполнить задание, добавляем в расписание
+    time.remove(tmp);
+    ans += tasks[i].cost;
+  }
+}
+ */
+/*Алгоритм:
+ - Задать два массива, с индексами и весами
+ - Пустить цикл внутри цикла, передавая соответствующий индекс
+ - Найти результат посредствам сравнения
+ */
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    //jkjk
     return 0;
 }
